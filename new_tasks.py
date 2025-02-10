304d9134a0c61d11dd1da16cfa9aafdb17194c2b
Marina Stakhovets <marina.stakhovets@gmail.com>
2025-02-10 10:53:39 +0300

    simple_task

---
+++ new_tasks.py
@@ -0,0 +1,32 @@
+### 1
+# str_1 = input('Enter first string: ')
+# str_2 = input('Enter second string: ')
+#
+# if str_1.lower() == str_2.lower():
+#     print('Strings is equals')
+# else:
+#     print('Strings is not equals')
+
+### 2
+# str_1 = input('Enter string: ')
+# print(str_1.swapcase()[::-1])
+
+### 3
+# import random
+#
+# list_ = [str(random.randint(1, 20)) for i in range(1, 10)]
+# print(list_)
+# print(''.join(list_))
+
+### 4
+# str_1 = input('Enter string: ')
+# print(''.join(sorted(str_1)))
+
+### 5
+# num = random.randint(1, 100)
+# print('Your num = ', num)
+#
+# if num % 2 == 0:
+#     print('Your new num = ', num * 5)
+# else:
+#     print('Your new num = ', num * 3)
